45d8647c947219f5bd96bb7d1371b0a54b8559f3
Brandon Silva | Cappta <brandonluizz@hotmail.com>
2018-04-18 22:45:55 -0300

    feat: added new ex files

--- ex/index.js
+++ ex/index.js
@@ -1,1 +1,12 @@
-console.log('Webpack');
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome;
+    }
+
+    toString() {
+        return `Pessoa: ${this.nome}`;
+    }
+}
+
+const pessoa = new Pessoa('Brandon');
+console.log(pessoa.nome);
